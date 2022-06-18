e2fefe95ecd750ed627cb60cbffcbc7fa637765b
remchaf <70279203+remchaf@users.noreply.github.com>
2022-06-18 22:35:28 +0000

    Got everything up to 'Implementation of Social Authentication' done ! Created auth.js and routes.js file to handle all stuffs related to passport settings and the routing logic respectively.

--- server.js
+++ server.js
@@ -1,21 +1,54 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+"use strict";
+require("dotenv").config();
+const routes = require("./routes");
+const auth = require("./auth.js");
+const ObjectID = require("mongodb").ObjectID;
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const { redirect } = require("express/lib/response");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
+// Setting the view-engine to pug
+app.set("view engine", "pug");
+
+// Express-session settings
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+// Passport settings
+app.use(passport.initialize());
+app.use(passport.session());
+
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render('Load your view here');
+myDB(async (client) => {
+  const myDataBase = await client.db("database").collection("Chat-Users");
+
+  //Instanciating routes and auth
+  routes(app, myDataBase);
+  auth(app, myDataBase);
+}).catch((e) => {
+  app.route("/").get((req, res) => {
+    res.render("pug", { title: e, message: "Unable to login" });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });
